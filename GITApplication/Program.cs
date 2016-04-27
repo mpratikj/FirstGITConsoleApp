5dde348c0fac552f77507636ad2166ca264d499b
pratikj <softwaredeveloper@yokotracking.com>
2016-04-27 10:05:17 +0400

    first commit

---
+++ GITApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Example App Demo !");  //Firstly written 
+            Console.ReadLine();
+        }
+    }
+}
